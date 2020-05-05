daa597a37e45792f4f70a2ac1f68ff82fe3eb7e4
koed <koed@L097105.corp.ads>
2020-05-04 21:04:33 -0400

    ver1

--- src/main/java/com/webapp/SpringBootWebApp.java
+++ src/main/java/com/webapp/SpringBootWebApp.java
@@ -2,11 +2,17 @@ package com.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebApp {
+public class SpringBootWebApp extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebApp.class, args);
-	}
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApp.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApp.class, args);
+    }
 }
